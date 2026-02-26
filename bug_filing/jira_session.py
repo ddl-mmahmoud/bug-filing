4e7138bc79a739da63ca44b15ba33de97c729aa3
Med Mahmoud <med.mahmoud@dominodatalab.com>
2026-02-26 11:53:58 +0000

    Allow override of JIRA_API_USERNAME and JIRA_API_PASSWORD

--- bug_filing/jira_session.py
+++ bug_filing/jira_session.py
@@ -7,10 +7,11 @@ def jira_base_url():
     return os.environ["JIRA_URL"].rstrip('/')
 
 
-def jira_requests_session():
+def jira_requests_session(username=None, password=None):
     session = requests.Session()
     auth = HTTPBasicAuth(
-        os.environ["JIRA_API_USERNAME"], os.environ["JIRA_API_PASSWORD"]
+        username or os.environ["JIRA_API_USERNAME"],
+        password or os.environ["JIRA_API_PASSWORD"],
     )
     headers = {"Accept": "application/json", "Content-Type": "application/json"}
     session.auth = auth
